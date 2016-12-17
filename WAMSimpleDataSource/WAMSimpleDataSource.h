0cb3ed3c1ba536ad4256d48b10b8ef40898aed11
WAMaker <wamakerdevelop@gmail.com>
2016-12-17 08:47:01 +0800

    Initial project

---
+++ WAMSimpleDataSource/WAMSimpleDataSource.h
@@ -0,0 +1,19 @@
+//
+//  WAMSimpleDataSource.h
+//  WAMSimpleDataSource
+//
+//  Created by wamaker on 2016/12/17.
+//  Copyright © 2016年 wamaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WAMSimpleDataSource.
+FOUNDATION_EXPORT double WAMSimpleDataSourceVersionNumber;
+
+//! Project version string for WAMSimpleDataSource.
+FOUNDATION_EXPORT const unsigned char WAMSimpleDataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WAMSimpleDataSource/PublicHeader.h>
+
+
